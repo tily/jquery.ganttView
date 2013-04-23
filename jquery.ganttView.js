8a22e569b24c7ec658823e94608bdb3c6aa598c6
OkawaUki <okawa.yuki@nifty.co.jp>
2013-04-23 11:53:46 +0900

    edit css

--- jquery.ganttView.js
+++ jquery.ganttView.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿?/*
 jQuery.ganttView v.0.8.8
 Copyright (c) 2010 JC Grubbs - jc.grubbs@devmynd.com
 MIT License Applies
@@ -148,12 +148,9 @@ behavior: {
                 }).append(data[i].project));
                 var seriesDiv = jQuery("<div>", { "class": "ganttview-vtheader-series" });
                 for (var j = 0; j < data[i].series.length; j++) {
-                    seriesDiv.append(jQuery("<div>", { "class": "ganttview-vtheader-series-name" }))
-                    	.append(data[i].series[j].name)
-						.append(jQuery("<div>", { "class": "ganttview-vtheader-series-memo" }))
-						.append(data[i].series[j].memo)
-						.append(jQuery("<div>", { "class": "ganttview-vtheader-series-member" }))
-						.append(data[i].series[j].member);
+                    seriesDiv.append(jQuery("<div>", { "class": "ganttview-vtheader-series-name" }).append(data[i].series[j].name))
+                        .append(jQuery("<div>", { "class": "ganttview-vtheader-series-memo" }).append(data[i].series[j].memo))
+						.append(jQuery("<div>", { "class": "ganttview-vtheader-series-member" }).append(data[i].series[j].member));
                 }
                 itemDiv.append(seriesDiv);
                 headerDiv.append(itemDiv);
