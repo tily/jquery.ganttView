c1a1be04c8adeee8be123bee560e1be28fd70a71
Okawa Yuki <okawa.yuki@nifty.co.jp>
2013-04-25 06:08:30 +0900

    edit js

--- gantt.js
+++ gantt.js
@@ -4,7 +4,14 @@ $(function () {
 		slideWidth: 900,
 		behavior: {
 			onClick: function (data) { 
+				$("#id > input").attr({value : data.id});
+				$("#prj > input").attr({value : data.project});
 				$("#name > input").attr({value : data.name});
+				$("#member > input").attr({value : data.member});
+				$("#start > input").attr({value : data.start.toString("yyyy-M-d")});
+				$("#end > input").attr({value : data.end.toString("yyyy-M-d")});
+				$("#progress > input").attr({value : data.progress});
+				$("#color > input").attr({value : data.color});
 			},
 			onResize: function (data) { 
 				var msg = "You resized an event: { start: " + data.start.toString("M/d/yyyy") + ", end: " + data.end.toString("M/d/yyyy") + " }";
@@ -17,8 +24,8 @@ $(function () {
 		}
 	});
 	
-	$('#submit').click(function () {
-		alert('test');
+	$('#submit').click(function (data) {
+		alert(data);
 	});
 	// $("#ganttChart").ganttView("setSlideWidth", 600);
 });
